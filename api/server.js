2663849c4fb63fd35fc0b1c9ef5af8c5c2c82b66
johnowow <oao_john@yahoo.com>
2025-09-02 21:56:54 -0700

    Update server.js to load creds from .env

--- api/server.js
+++ api/server.js
@@ -6,13 +6,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+require('dotenv').config();
+
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'hw3',
-    password: 'cse135!!UCSD',
-    database: 'cse135_hw3',
-    connectionLimit: 5
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  connectionLimit: 5,
+  ssl: { minVersion: 'TLSv1.2' }
 });
+
+
 
 async function q(sql, params=[]) {
     const [rows] = await pool.query(sql, params);
